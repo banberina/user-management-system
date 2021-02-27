5aebeb1e53598c0c5125e2952e711658407ebee9
banberina <berina.bandic@gmail.com>
2021-02-27 01:30:23 +0100

    error 404 page

--- client/src/pages/404/not-found.component.jsx
+++ client/src/pages/404/not-found.component.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Jumbotron, Container } from "reactstrap";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>Not found!</h1>
-        </div>
-    )
-}
+  return (
+    <Jumbotron fluid>
+      <Container color='info'>
+        <h1 className="special-font">Error 404: Page not found</h1>
+      </Container>
+    </Jumbotron>
+  );
+};
 
-export default NotFound
+export default NotFound;
