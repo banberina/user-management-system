2bd64de66c02c97e526f738b8b9554860b7d8904
banberina <berina.bandic@gmail.com>
2021-03-01 22:31:54 +0100

    card design modified.

--- client/src/components/card/card.component.jsx
+++ client/src/components/card/card.component.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { Button } from "reactstrap";
+import { Button, Card } from "reactstrap";
 
 import "./card.styles.css";
 
-const Card = ({ name, parameter1, parameter2, img, del, id, edit }) => (
-  <div className="card-container">
-    <Button className="special-font-subheader" color="info" href={edit}>Edit</Button>
-    <Button className="special-font-subheader" color="danger" onClick={()=>del(id)}>Delete</Button>
+const CardComponent = ({
+  name,
+  parameter1,
+  parameter2,
+  img,
+  del,
+  id,
+  edit,
+}) => (
+  <Card className="card-container" style={{ backgroundColor: "#95dada" }}>
+    <Button className="special-font-subheader" color="info" href={edit}>
+      Edit
+    </Button>
+    <Button
+      className="special-font-subheader"
+      color="danger"
+      onClick={() => del(id)}
+    >
+      Delete
+    </Button>
     <img alt="img" src={img} />
     <h2 className="special-font"> {name} </h2>
     <h3 className="special-font-subheader"> {parameter1} </h3>
     <h3 className="special-font-subheader"> {parameter2} </h3>
-  </div>
+  </Card>
 );
 
-export default Card;
+export default CardComponent;
